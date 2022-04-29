0f4583b77c5aac2809b61278441dbe04dc3045f3
Vladyslav Hnatovskyi <vgtsky@gmail.com>
2022-04-29 16:05:51 +0300

    Update unique-id

--- src/unique-id.ts
+++ src/unique-id.ts
@@ -1,19 +1,19 @@
 import { ObjectType } from './types';
 
 const idCounter: ObjectType = {}
-const lodashPrefix: string = '$lodash$';
+const vhdashPrefix: string = '$vhdash$';
 
 /**
  *
  * @param {string} prefix The value to prefix the ID with
  * @returns {string} Returns the unique ID
  */
-const uniqueId = (prefix: string = lodashPrefix): any => {
+const uniqueId = (prefix: string = vhdashPrefix): any => {
   if (!idCounter[prefix]) idCounter[prefix] = 0;
 
   const id: number = ++idCounter[prefix];
 
-  if (prefix === lodashPrefix) return id;
+  if (prefix === vhdashPrefix) return id;
 
   return `${prefix}${id}`;
 };
